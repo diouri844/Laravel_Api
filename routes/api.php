f969873caf26314aa59a71f0a9e605adb9af3893
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-11-01 21:13:09 +0100

    update Laravel setup

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// create new router :
+
+Route::get('/Products', function(){ return []; });
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
